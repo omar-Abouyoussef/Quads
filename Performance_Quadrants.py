c07df52ecdfc4e4fb9cc17a87202c9c26e972afb
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-10 22:34:27 +0200

    Update Performance_Quadrants.py

--- Performance_Quadrants.py
+++ Performance_Quadrants.py
@@ -156,7 +156,7 @@ tickers = st.text_input(label='Ticker(s)',
                         value=" ".join(factors.index.to_list()),
                         key='tickers',
                         help="Enter all uppercase!")
-if tickers == "":
+if tickers is None:
     tickers = " ".join(factors.index.to_list())
     tickers = st.session_state.tickers
 else:
