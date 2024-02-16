a4151159b4d737b66d4266bccd1ce2298664da85
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-16 20:32:55 +0200

    Update Model_Diagnostics.py

--- pages/Model_Diagnostics.py
+++ pages/Model_Diagnostics.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from factor_analyzer import ConfirmatoryFactorAnalyzer, ModelSpecificationParser
-from Performance_Quadrants import performance, loadings
-model_dict = {"Short-term": ["1-Day", "2-Day", "3-Day"],
-"Medium-term": ["1-Week", "2-Week", "3-Week"],
-"Long-term": ["1-Month", "3-Month", "6-Month"]}
+from Performance_Quadrants import performance, cfa
 
-model_spec = ModelSpecificationParser.parse_model_specification_from_dict(performance, model_dict)
-cfa = ConfirmatoryFactorAnalyzer(model_spec)
-cfa.fit(performance.values)
+def goodness_of_fit(X):
+    return np.sum(np.sum(X**2, axis=1))/X.shape[0]
+
+
 loadings = pd.DataFrame(cfa.loadings_, index=performance.columns, columns=["Short-term","Medium-term", "Long-term"])
-print(loadings.round(3))
-print(goodness_of_fit(loadings).round(3))
+st.table(loadings.round(3))
+st.write(goodness_of_fit(loadings).round(3))
