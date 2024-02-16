28736e105c37440d2669350be204b5d162071c24
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-16 20:29:41 +0200

    Update and rename Model Diagnostics.py to Model_Diagnostics.py

--- pages/Model_Diagnostics.py
+++ pages/Model_Diagnostics.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from factor_analyzer import ConfirmatoryFactorAnalyzer, ModelSpecificationParser
-from Quads
+from Performance_Quadrants import performance, loadings
 model_dict = {"Short-term": ["1-Day", "2-Day", "3-Day"],
 "Medium-term": ["1-Week", "2-Week", "3-Week"],
 "Long-term": ["1-Month", "3-Month", "6-Month"]}
