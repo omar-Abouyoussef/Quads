3e095b86754a78fc755af3a04f1ad7dd46a5a40a
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-24 00:34:34 +0200

    Update Model_Diagnostics.py

--- pages/Model_Diagnostics.py
+++ pages/Model_Diagnostics.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from Performance_Quadrants import performance, cfa
 
 def goodness_of_fit(X):
     return np.sum(np.sum(X**2, axis=1))/X.shape[0]
 
 
-loadings=pd.DataFrame(cfa.loadings_,index=performance.columns,columns=["Short-term","Medium-term","Long-term"])
+loadings=pd.DataFrame(st.session_state.cfa.loadings_,index=st.session_state.performance.columns,columns=["Short-term","Medium-term","Long-term"])
 st.header("Factor Loadings")
 st.table(loadings.round(3))
 st.write(f"Goodness of fit: {(goodness_of_fit(loadings)*100).round(3)}%")
