3104c128f7ce697e5b56a6759054ec7917e38e27
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-29 14:40:07 +0200

    Update Model_Diagnostics.py

--- pages/Model_Diagnostics.py
+++ pages/Model_Diagnostics.py
@@ -6,7 +6,8 @@ def goodness_of_fit(X):
     return np.sum(np.sum(X**2, axis=1))/X.shape[0]
 
 
-loadings=pd.DataFrame(st.session_state.cfa.loadings_,index=st.session_state.performance.columns,columns=["Short-term","Medium-term","Long-term"])
+loadings=pd.DataFrame(st.session_state.cfa.loadings_,index=st.session_state.performance.columns,columns=["Factor 1","Fctor 2","Factor 3"])
+loadings['Communalities'] = cfa.get_communalities()
 st.header("Factor Loadings")
 st.table(loadings.round(3))
 st.write(f"Goodness of fit: {(goodness_of_fit(loadings)*100).round(3)}%")
