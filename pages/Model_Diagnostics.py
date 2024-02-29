38c244517032d497886c93322090c4d1ad17094b
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-02-29 14:51:10 +0200

    Update Model_Diagnostics.py

--- pages/Model_Diagnostics.py
+++ pages/Model_Diagnostics.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 def goodness_of_fit(X):
-    return np.sum(X))/X.shape[0]
+    return np.sum(X)/X.shape[0]
 
 
 loadings=pd.DataFrame(st.session_state.cfa.loadings_,index=st.session_state.performance.columns,columns=["Factor 1","Fctor 2","Factor 3"])
