83e0e64180bf8b9b7c4564034b3e6f60e7072159
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-03-11 17:33:06 +0200

    Update Performance_Quadrants.py

--- Performance_Quadrants.py
+++ Performance_Quadrants.py
@@ -347,7 +347,7 @@ else:
                             ),
                             marker=dict(
                                 symbol="arrow",
-                                size=5,
+                                size=8,
                                 angleref="previous",
                             ),
                             name=ticker
@@ -369,7 +369,7 @@ else:
                             ),
                             marker=dict(
                                 symbol="arrow",
-                                size=5,
+                                size=8,
                                 angleref="previous",
                             ),
                             name=ticker
@@ -390,7 +390,7 @@ else:
                             ),
                             marker=dict(
                                 symbol="arrow",
-                                size=5,
+                                size=8,
                                 angleref="previous",
                             ),
                             name=ticker
