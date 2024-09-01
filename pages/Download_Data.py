d498ecbd1bd388e3c671b5af9e05a609c826774a
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-09-01 14:33:19 +0300

    Update fx Download_Data.py

--- pages/Download_Data.py
+++ pages/Download_Data.py
@@ -8,13 +8,16 @@ import io
 
 @st.cache_data
 def download(ticker, market, start, end, key):
-    try:
-      url = f'https://eodhd.com/api/eod/{ticker}.{market}?from={start}&to={end}&filter=close&period=d&api_token={key}&fmt=json'
-      res = requests.get(url)
-      if res.status_code == 200:
-        close = requests.get(url).json()
-    except:
-      pass
+    if market in ["US", 'FOREX']:
+        close = pdr.get_data_yahoo(stock_list, start, end)["Close"]
+    else:
+        try:
+            url = f'https://eodhd.com/api/eod/{ticker}.{market}?from={start}&to={end}&filter=close&period=d&api_token={key}&fmt=json'
+            res = requests.get(url)
+            if res.status_code == 200:
+                close = requests.get(url).json()
+        except:
+            pass
     return ticker, close
 
 @st.cache_data
