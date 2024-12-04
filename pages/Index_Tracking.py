ea022fba512a19b9ef8fb808bea2341616ba2171
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-12-04 23:19:22 +0200

    Update Index_Tracking.py

--- pages/Index_Tracking.py
+++ pages/Index_Tracking.py
@@ -220,7 +220,7 @@ col1, col2 = st.columns([0.7,0.3])
 with col1:
     fig = go.Figure(
     data=go.Heatmap(
-                     z=(params.T.iloc[:,-10:]>0).astype(int), coloraxis="coloraxis",
+                     z=(params.T.iloc[:,-30:]>0).astype(int), coloraxis="coloraxis",
         x=params.index, y=params.columns, colorscale=[[0,'rgb(239,35,60)'],[1,'rgb(72,202,228)']],
         xgap=4,ygap=4
 )
