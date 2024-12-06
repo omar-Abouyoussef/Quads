52da896b1cdd9c327c067efa1f4c5777861b007b
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-12-06 14:26:45 +0200

    Update Index_Tracking.py

--- pages/Index_Tracking.py
+++ pages/Index_Tracking.py
@@ -254,7 +254,7 @@ st.plotly_chart(fig)
 ##################
 
 alltime_zero_weight_tickers= ((weights==0.0).sum()==weights.shape[0]).index.to_list()
-st.write(weights == 0.0).sum())
+st.write((weights == 0.0).sum())
 Weights=weights.drop(alltime_zero_weight_tickers, axis=1)
 
 col1, col2 = st.columns([0.8,0.1])
