87c52d29017136e66ce0c446ffb0914dc4abd943
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2024-12-06 10:08:00 +0200

    Update Index_Tracking.py

--- pages/Index_Tracking.py
+++ pages/Index_Tracking.py
@@ -214,7 +214,7 @@ for i in range(0, len(X) - window_size + 1,  rebalance):
     score.append(model.score(X_window,y_window))
     date.append(X_window.index[-1])
 params = pd.DataFrame(coefs, index=date)
-
+st.write(params)
 weights = params.apply(lambda x: abs(x)/abs(x).sum(), axis=1)
 
 weights.index.name='Date'
