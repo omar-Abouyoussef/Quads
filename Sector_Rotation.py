9841252c083dca847b759fae290fbc0a8f6516a5
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-01-16 16:59:23 +0200

    Update Sector_Rotation.py

--- Sector_Rotation.py
+++ Sector_Rotation.py
@@ -121,8 +121,8 @@ st.set_page_config(page_title="Sector Rotation", layout='wide')
 st.title('Sector Rotation')
 
 pages = [st.Page("Sector_Rotation.py", title="Sector Rotation"),
-         st.Page("pages/Sector Analysis.py", title="Sector Analysis"),
-        st.Page("pages/Market_Interaction.py", title="Market interaction")]
+         st.Page("Sector Analysis.py", title="Sector Analysis"),
+        st.Page("Market_Interaction.py", title="Market interaction")]
 
 pg = st.navigation(pages)
 pg.run()
