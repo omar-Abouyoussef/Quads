d17309d4c0a3de002798dcffdd1e8b15786e1b9e
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-02-17 20:15:05 +0200

    Update Sector_Rotation.py

--- Sector_Rotation.py
+++ Sector_Rotation.py
@@ -261,6 +261,9 @@ else:
     df_50_100.set_index(longterm.index, inplace=True)
     df_50_100 = df_50_100[['Medium-term','Long-term', 'Sector']]
 
+df_20_50 = df_20_50.apply(denoise,axis =1)
+df_50_100 = df_50_100.apply(denoise,axis =1)
+
 st.session_state.df_20_50 = df_20_50
 st.session_state.df_50_100 = df_50_100
 
