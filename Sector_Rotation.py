bf884071d4debd6e9f2ad31aafa9721d4bd268b6
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-02-22 22:00:49 +0200

    Update Sector_Rotation.py

--- Sector_Rotation.py
+++ Sector_Rotation.py
@@ -174,7 +174,7 @@ fig2 = go.Figure(
                    )]
     )
 fig2. update_layout(showlegend=False)
-fig2.layout.template="simple_white"
+fig2.layout.template="plotly"
 
 if market == 'america':
 
