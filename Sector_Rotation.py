06f97e3ad1307e50ad75aee8a03bcbf57d0f4ca3
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-02-22 21:59:52 +0200

    Update Sector_Rotation.py

--- Sector_Rotation.py
+++ Sector_Rotation.py
@@ -174,7 +174,7 @@ fig2 = go.Figure(
                    )]
     )
 fig2. update_layout(showlegend=False)
-fig2.layout.template="seaborn"
+fig2.layout.template="simple_white"
 
 if market == 'america':
 
