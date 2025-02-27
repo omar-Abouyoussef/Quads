0a7e06e0e9073ac2555249b61baf1b4619a43059
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-02-27 16:53:54 +0200

    Update Sector_Rotation.py

--- Sector_Rotation.py
+++ Sector_Rotation.py
@@ -7,6 +7,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import statsmodels.api as sm
+from scipy.signal import savgol_filter
 from retry import retry
 import streamlit as st
 
