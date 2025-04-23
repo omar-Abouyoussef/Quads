73c55c80c3e6e2c5a6a287b592c1060d1d97395e
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-04-23 20:29:45 +0200

    Update Download_Data.py

--- pages/Download_Data.py
+++ pages/Download_Data.py
@@ -46,14 +46,16 @@ end = st.session_state.end
 date = dt.today().date()
 
 if start < end:
+    start = time.time()
     if interval in ['1 Minute','5 Minute','30 Minute']:
             df = get_EGX_intraday_data(tickers.split(" "),interval,start,end)
 
     else:
         df = eod_cache_func(tickers.split(" "),interval,start,end,date)
         df.index = df.index.date
-    
+    end = time.time()
     st.write(df)
+    st.write(f'{end-start}:.2f seconds')
     st.write(f"Samples:{df.shape[0]}")
 
 # Download Button
